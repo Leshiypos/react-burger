e26e8663e365ad79dd6187cde1a39430a3c669f3
Wp_devsite <leshiy_pos@mail.ru>
2024-12-21 11:26:15 +0300

    Added event of closing modal window by esc button

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,10 +1,20 @@
 import styles from "./modal.module.css";
 import { createPortal } from "react-dom";
+import { useEffect } from "react";
 
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import ModalOverlay from "./modal-overlay";
 
 export default function Modal({ isOpen, onClick, children }) {
+  useEffect(() => {
+    const closeModal = (event) => {
+      if (event.key === "Escape") onClick(false);
+      console.log("render");
+    };
+    isOpen
+      ? document.addEventListener("keydown", closeModal)
+      : document.removeEventListener("keydown", closeModal);
+  }, [isOpen]);
   return createPortal(
     <>
       {isOpen && (
