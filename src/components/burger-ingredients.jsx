015b553db823207036934aea618ea9f344ff28f4
Wp_devsite <leshiy_pos@mail.ru>
2025-01-10 19:33:31 +0300

    Added function change current tab

--- src/components/burger-ingredients.jsx
+++ src/components/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import IndredientsCategory from "./ingredients-category";
 import styles from "./burger-ingredients.module.css";
@@ -10,17 +10,36 @@ import { getIngredientsByBategories } from "../services/ingredients/selectors";
 
 export default function BurgerIngredients() {
   const [tab, setTab] = useState("buns");
+  const tabRef = useRef();
+  const bunsRef = useRef();
+  const saucesRef = useRef();
+
   const { details } = useSelector(getDetails);
   const { buns, mains, sauces } = useSelector(getIngredientsByBategories);
 
+  const handleScrollСhangeTab = () => {
+    const bunsDef =
+      bunsRef.current.getBoundingClientRect().bottom -
+      tabRef.current.getBoundingClientRect().bottom;
+    const saucesDef =
+      saucesRef.current.getBoundingClientRect().bottom -
+      tabRef.current.getBoundingClientRect().bottom;
+
+    bunsDef > 0
+      ? setTab("buns")
+      : saucesDef > 0
+      ? setTab("sauces")
+      : setTab("mains");
+  };
+
   return (
     <div className={styles.burger_ingredients}>
-      <Tabs active={tab} onChange={(current) => setTab(current)} />
-      <ul className={styles.work_area}>
-        <li>
+      <Tabs active={tab} onChange={(current) => setTab(current)} ref={tabRef} />
+      <ul className={styles.work_area} onScroll={handleScrollСhangeTab}>
+        <li ref={bunsRef}>
           <IndredientsCategory title="Булки" ingredients={buns} />
         </li>
-        <li>
+        <li ref={saucesRef}>
           <IndredientsCategory title="Соусы" ingredients={sauces} />
         </li>
         <li>
--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -1,22 +1,24 @@
 import styles from "./tabs.module.css";
 import PropTypes from "prop-types";
 import Tab from "./tab";
+import React from "react";
 
-export default function Tabs({ active, onChange }) {
+const Tabs = React.forwardRef(({ active, onChange }, ref) => {
   return (
-    <ul className={styles.tabs}>
+    <ul className={styles.tabs} ref={ref}>
       <Tab isActive={active === "buns"} onClick={() => onChange("buns")}>
         Булки
       </Tab>
       <Tab isActive={active === "sauces"} onClick={() => onChange("sauces")}>
         Соусы
       </Tab>
-      <Tab isActive={active === "filling"} onClick={() => onChange("filling")}>
+      <Tab isActive={active === "mains"} onClick={() => onChange("mains")}>
         Начинки
       </Tab>
     </ul>
   );
-}
+});
+export default Tabs;
 
 Tabs.propTypes = {
   active: PropTypes.string.isRequired,
